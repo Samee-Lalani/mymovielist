05e413c6f968eef71abe8adfb529aca3b3d1ba2d
Raegan Girdley <102489778+raegangirdley@users.noreply.github.com>
2023-04-15 20:47:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-import React, { useState } from 'react';
 import {Route, Routes} from 'react-router-dom';
 import Desktop1 from "./pages/Desktop1.js";
 import Desktop2 from './pages/Desktop2.js';
+import Desktop3 from './pages/Desktop3.js';
 import ErrorPage from "./pages/ErrorPage.js";
+import SignoutPage from "./pages/SignoutPage.js";
 
 export default function App() {
   
@@ -12,9 +12,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Desktop1 /> }  />
         <Route path="/sign-in" element={<Desktop2 />} />
+        <Route path="/logged-in" element={<Desktop3 />} />
+        <Route path="/signed-out" element={<SignoutPage />} />
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
 }
-
