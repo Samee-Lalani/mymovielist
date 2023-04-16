1b8b3c60c518546c066cce50042ced2bf7222302
Raegan Girdley <102489778+raegangirdley@users.noreply.github.com>
2023-04-16 13:22:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import {Route, Routes} from 'react-router-dom';
 import Desktop1 from "./pages/Desktop1.js";
 import Desktop2 from './pages/Desktop2.js';
-import Desktop3 from './pages/Desktop3.js';
 import ErrorPage from "./pages/ErrorPage.js";
 import SignoutPage from "./pages/SignoutPage.js";
+import { useState } from 'react';
 
 export default function App() {
-  
+
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+
+  const handleSignIn = () => {
+    setisLoggedIn(true);
+  };
+
+  const handleSignOut = () => {
+    setisLoggedIn(false);
+  }
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Desktop1 /> }  />
-        <Route path="/sign-in" element={<Desktop2 />} />
-        <Route path="/logged-in" element={<Desktop3 />} />
+        <Route path="/" element={<Desktop1 isLoggedIn={isLoggedIn} handleSignOut={handleSignOut} /> }  />
+        <Route path="/sign-in" element={<Desktop2 handleSignIn={handleSignIn}/>} />
         <Route path="/signed-out" element={<SignoutPage />} />
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
