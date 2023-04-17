4d13625637ce100216891958691d1059202c5616
Raegan Girdley <102489778+raegangirdley@users.noreply.github.com>
2023-04-16 21:11:22 -0400

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,51 @@
-const Cards = (props) => {
-    var card = props.user_list;
+import './Cards.css';
+import { useState } from 'react';
+
+export default function Cards(props) {
+
+    const [showForm, setShowForm] = useState(false);
+
+    const handleClick = () => {
+        setShowForm(true);
+    };
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    };
+
     return (
-        <div className="userslist">
-                <p className="name">{user_list.name}</p>
-                <p className="description">{user_list}</p>
+        <div className="flex_box">
+            <div className="box">
+                <div className="person" key = {props.id}>
+                    <p className="name">{props.name}</p>
+                    <img src={props.image} alt={props.name} className="personsimage" />
+                    <p className="description">{props.description}</p>
+                    {props.isLoggedIn ? (
+                        <>
+                        <button id="edit-button" onClick={handleClick}>Edit</button>
+                        <br /> <br />
+                        {showForm && (
+                            <form className="popupForm" onSubmit={handleSubmit}>
+                                <label for="newTitle">Title: </label>
+                                <input type="text" id="nameInput" name="newTitle" />
+                                <br />
+                                <label for="newImg">Image: </label>
+                                <input type="image" name="newImage" alt="Insert Image" />
+                                <br />
+                                <label for="newDesc">Description: </label>
+                                <input type="text" id="descInput" name="newDesc" />
+                                <br /><br />
+                                <input id="submitForm" type="submit" value="Submit" />
+                                <br /> <br />
+                            </form>
+                        )}
+                        </>
+                    ) : (
+                        <>
+                        </>
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
-export default Cards;
